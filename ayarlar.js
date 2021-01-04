42e74745d1bf51e637fac49c705fa260a298a3c5
Utku281 <75776573+Utku281@users.noreply.github.com>
2021-01-04 20:54:28 +0300

    Update ayarlar.js

--- ayarlar.js
+++ ayarlar.js
@@ -1,5 +1,5 @@
 module.exports = {
-  "token": "BOTUNUZUN TOKENİ",
-  "sahip": "SAHİP ID",
-  "prefix": "BOTUNUZUN PREFİXİ"
+  "token": "Nzk0NDQ1ODk3ODk1MjQ3ODcy.X-67Zg.IudFRMwM7_bJloamfIDLKsRxY9M",
+  "sahip": "765277292888195184",
+  "prefix": "s!
 };
